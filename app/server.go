3bbf13881c946f2e9a6d9195b90b53e6781245fa
SimoneDutto <Simone.Dutto@leitha.eu>
2024-06-18 14:54:32 +0200

    add echo route

--- app/server.go
+++ app/server.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
+	"net/http"
+	"path"
 	"strings"
 
 	// Uncomment this block to pass the first stage
 	"net"
 	"os"
 )
+
+type StatusLine struct {
+	Method        string
+	RequestTarget string
+	HttpVersion   string
+}
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -21,7 +31,6 @@ func main() {
 		os.Exit(1)
 	}
 	conn, err := l.Accept()
-	defer conn.Close()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
@@ -32,22 +41,85 @@ func main() {
 		fmt.Println("Error reading from connection")
 		os.Exit(1)
 	}
-	lines := strings.Split(string(b), "\r\n")
-	if len(lines) > 0 {
-		reqLines := strings.Fields(lines[0])
-		fmt.Println(reqLines)
-		fmt.Println(len(reqLines))
-		if len(reqLines) < 2 {
-			fmt.Println("req line bad formatted")
+	httpRequestParts := strings.Split(string(b), "\r\n")
+	if len(httpRequestParts) > 0 {
+		statusLine, err := parseStatusLine(httpRequestParts[0])
+		header := parseHeader(httpRequestParts[1:])
+		_ = header
+		if err != nil {
+			fmt.Println(err)
 			os.Exit(1)
 		}
-		if reqLines[0] == "GET" && reqLines[1] == "/" {
-			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		if statusLine.Method == "GET" && statusLine.RequestTarget == "/" {
+			response := http.Response{
+				Status:     "200 OK",
+				StatusCode: 200,
+				Proto:      "HTTP/1.1",
+				ProtoMajor: 1,
+				ProtoMinor: 1,
+			}
+			err := response.Write(conn)
+			if err != nil {
+				fmt.Println("Error during write", err)
+			}
+		} else if statusLine.Method == "GET" && strings.HasPrefix(statusLine.RequestTarget, "/echo") {
+			echoString := path.Base(statusLine.RequestTarget)
+			stringReader := strings.NewReader(echoString)
+			stringReadCloser := io.NopCloser(stringReader)
+			h := http.Header{}
+			h.Add("Content-Type", "text/plain")
+			h.Add("Content-Lenght", fmt.Sprint(len(echoString)))
+			response := http.Response{
+				Status:     "200 OK",
+				StatusCode: 200,
+				Proto:      "HTTP/1.1",
+				ProtoMajor: 1,
+				ProtoMinor: 1,
+				Header:     h,
+				Body:       stringReadCloser,
+			}
+			err := response.Write(conn)
+			if err != nil {
+				fmt.Println("Error during write", err)
+			}
 		} else {
-			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+			response := http.Response{
+				Status:     "404 Not Found",
+				StatusCode: 404,
+				Proto:      "HTTP/1.1",
+				ProtoMajor: 1,
+				ProtoMinor: 1,
+			}
+			err := response.Write(conn)
+			if err != nil {
+				fmt.Println("Error during write", err)
+			}
 		}
 	} else {
 		fmt.Println("Bad formatted request")
 		os.Exit(1)
 	}
+}
+
+func parseHeader(header []string) http.Header {
+	h := http.Header{}
+	for _, v := range header {
+		hFields := strings.Split(v, ": ")
+		if len(hFields) == 2 {
+			h.Add(hFields[0], hFields[1])
+		}
+	}
+	return h
+}
+
+func parseStatusLine(statusLine string) (*StatusLine, error) {
+	statusLineFields := strings.Fields(statusLine)
+	if len(statusLineFields) < 3 {
+		return nil, errors.New("Error trying to parse the status line")
+	}
+	return &StatusLine{
+		Method:        statusLineFields[0],
+		RequestTarget: statusLineFields[1],
+		HttpVersion:   statusLineFields[2],
+	}, nil
 }
